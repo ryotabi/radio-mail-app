49a0e02e3da133a0bba6a399df4f834f2748e5d9
瀬川椋太 <ryota-segawe@sekawamukufutoshinoMacBook-Pro.local>
2021-06-05 21:00:36 +0900

    開発環境用のApi追加

--- src/config/RadioMailApiData.ts
+++ src/config/RadioMailApiData.ts
@@ -1,5 +1,5 @@
 // 開発環境用
-const RadioMailAPIURL = 'http://s-ryota.sakura.ne.jp/radioMailAPI_dev/'
+const RadioMailAPIURL = 'http://s-ryota.sakura.ne.jp/radioMailAPI_dev'
 // 本番環境用
 // const RadioMailAPIURL = 'https://radiomailer.site'
 
